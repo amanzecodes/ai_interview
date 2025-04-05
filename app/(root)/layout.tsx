c5cc2b4a10255653cff3e291b4b1d45deb281dce
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-04-05 20:39:02 +0100

    sall changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
-
 const Rootlayout = async  ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated()
 
