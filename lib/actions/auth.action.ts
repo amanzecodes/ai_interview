297fb852099c1abf45455a10c44b2f7c9be00efa
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-04-07 08:52:42 +0100

    implemented new action for fetching generated interview form firebase db

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -130,3 +130,12 @@ export async function isAuthenticated() {
   const user = await getCurrentUser();
   return !!user;
 }
+
+export async function getInterviewByUserId(userId: string):Promise<Interview [] | null> {
+    const interviews = await db.collection('interview').where("userid", '==', userId).orderBy('createdAt', 'desc').get();
+
+    return interviews.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data
+    })) as Interview[];
+}
